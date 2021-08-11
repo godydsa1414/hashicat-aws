d6cfe8584b30c9e979ba0cd972a977442978a7f4
godwin <gdsouza@wi.rr.com>
2021-08-11 19:02:42 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -128,13 +128,13 @@ resource "aws_instance" "hashicat" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.hashicat.id
-  department                  = "devops"
-  billable                    = "true"
+  subnet_id                   = aws_subnet.hashicat.id 
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    department                  = "devops"
+    billable                    = "true"
   }
 }
 
